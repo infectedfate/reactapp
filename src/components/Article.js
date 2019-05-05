53cf3a5ef820c51922993b29e159c6e3cdb4235f
Malinin Mikhail <infectedfate@gmail.com>
2019-05-06 00:19:55 +0300

    adding a button and statement

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-    const {article} = props
-    const body = <section>{article.text}</section>
-    const creation_date = new Date(article.date).toDateString() 
-    return(
-        <div>
-            <h2>
-            {article.title}
-            
-            </h2>
-            {body}
-            <h3>Creation date: {creation_date}</h3>
-        </div>
-    )
+class Article extends Component {
+    state = {
+        isOpen: true
+    }
+
+    render() {
+        const {article} = this.props
+        const body = this.state.isOpen && <section>{article.text}</section>
+        const creation_date = new Date(article.date).toDateString() 
+        return(
+            <div>
+                <h2>
+                {article.title}
+                <button onClick={this.handleClick}>
+                {this.state.isOpen ? 'close' : 'open'}
+                </button>
+                </h2>
+                {body}
+                <h3>Creation date: {creation_date}</h3>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        console.log('---', 'clicked')
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
+
 
 export default Article
