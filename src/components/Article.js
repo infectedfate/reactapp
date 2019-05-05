f897acb3572b8af5db1e6c5df48588563fec1e58
Malinin Mikhail <infectedfate@gmail.com>
2019-05-06 01:30:04 +0300

    including bootstrap

--- src/components/Article.js
+++ src/components/Article.js
@@ -7,18 +7,24 @@ class Article extends Component {
 
     render() {
         const {article} = this.props
-        const body = this.state.isOpen && <section>{article.text}</section>
+        const body = this.state.isOpen && <section className="card-text">{article.text}</section>
         const creation_date = new Date(article.date).toDateString() 
         return(
-            <div>
-                <h2>
-                {article.title}
-                <button onClick={this.handleClick}>
-                {this.state.isOpen ? 'close' : 'open'}
-                </button>
-                </h2>
-                {body}
-                <h3>Creation date: {creation_date}</h3>
+            <div className="card mx-auto" style={{ width: '50%' }}>
+                <div className="card-header">
+                    <h2>
+                        {article.title}
+                        <button onClick={this.handleClick} className="btn btn-primary btn-lg float-right">
+                        {this.state.isOpen ? 'close' : 'open'}
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-subtitle text-muted">
+                        Creation date: {creation_date}
+                    </h6>
+                    {body}
+                </div>
             </div>
         )
     }
